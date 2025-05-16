24b9e6f8b775977fa193506f94022e88bf5509d7
Spencer Smolen <45985425+kriipke@users.noreply.github.com>
2025-05-16 06:23:15 -0400

    Add files via upload

---
+++ web/src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import { ChartpressHelmChartWizard } from "./ChartpressHelmChartWizard/ChartpressHelmChartWizard";
+
+export default function App() {
+  return (
+    <div>
+      <ChartpressHelmChartWizard />
+    </div>
+  );
+}
